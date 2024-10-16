489453f1c9354b4a2a2f92b32788fe0e944d6021
Nishant Balepur <55101514+nbalepur@users.noreply.github.com>
2024-10-16 12:52:00 -0400

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
   $('a.abstract').click(function() {
     $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
+  });
+  $('a.tldr').click(function() {
+    $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
   });
   $('a.bibtex').click(function() {
     $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
   });
   $('a').removeClass('waves-effect waves-light');
 
